ef613937d6d53510cc158d9b173e58d45367c6c7
Amit <rajamitnine@gmail.com>
2022-08-25 20:20:07 +0530

    Parameter destructuring that is used a lot in React

--- file53.js
+++ file53.js
@@ -1,4 +1,29 @@
 // Parameter Destructuring:
 
-const func2 = num => num % 2 === 0;
-console.log(func2(4));
+// Object
+// React
+
+const person = {
+    firstName: 'Amit',
+    gender: 'Male',
+    age: 23
+};
+
+function getDetails(obj) {
+    console.log(obj.firstName);
+    console.log(obj["gender"]);
+    console.log(obj.age);
+    console.log(obj.address);
+};
+getDetails(person);
+
+// Or,
+
+function printDetails({firstName: name, age, gender}) {
+    console.log(name);
+    console.log(gender);
+    console.log(age);
+};
+
+printDetails(person);
+
