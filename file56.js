242e7f46e8f68cd13e3950634afca6091854dca3
Amit <rajamitnine@gmail.com>
2022-08-26 17:32:53 +0530

    map() method of an Array

--- file56.js
+++ file56.js
@@ -11,7 +11,7 @@ const myFunc = (number, index) => {
 // };
 
 
-// forEach():
+// forEach() method:
 // forEach() method takes argument as a callback function.
 
 numbers.forEach(myFunc);
@@ -68,11 +68,55 @@ users.forEach((user, index) => {console.log(`${user["firstName"]}, Index is ${in
 //     console.log(user.firstName);
 // };
 
-// map(), vry useful in React:
+// map() method, vry useful in React:
+// map method also takes callback function as an input.
+// The callback function must always use the 'return' statement as it always gives a 'new array' as an output.
 
+const ints = [6, 8, 5, 4];
 
+const square = function(number) {
+    return (number ** 2);
+};
 
-// filter():
+const squareNumber = ints.map(square);
+console.log(squareNumber);
+
+// Or,
+
+const cubeOfNumber = ints.map((number, index) => (`${number ** 3}, Index is ${index}.`));
+console.log(cubeOfNumber);
+
+// Or,(Real-world application):
+
+const customers = [
+    {firstName: 'Amit', age: 25},
+    {firstName: 'John', age: 22},
+    {firstName: 'Harshit', age: 23},
+    {firstName: 'Nitish', age: 21},
+    {firstName: 'Garima', age: 18}
+];
+
+const customerNames = customers.map(customer => (customer["firstName"]));
+console.log(customerNames);
+
+// Thus:
+const customerName = (customerFirstName) => {
+    console.log(`Costumer's Name: ${customerFirstName}`);
+};
+
+customerNames.forEach(customerName);
+
+// Or,
+
+customerNames.forEach(customerName => {console.log(`firstName of the customer: ${customerName}.`);});
+
+// Alternate:
+
+for(let customerName of customerNames) {
+    console.log(customerName);
+};
+
+// filter() function:
 
 
 
